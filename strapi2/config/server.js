d186dd08c662545b9bca98e048a95a61cf46b1be
raianegomes1805 <64805607+raianegomes1805@users.noreply.github.com>
2021-07-19 14:54:24 -0400

    Add files via upload

---
+++ strapi2/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7b768da6bcc91a8674f39ff9d95fe630'),
+    },
+  },
+});
---
+++ strapi2/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9ddea053-1cd7-4731-b16a-f0524f17d5ab'
+};
